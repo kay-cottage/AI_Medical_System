6025280236a8a00050c35fc952f378701ff24981
kay的小屋 <47267728+kay-cottage@users.noreply.github.com>
2022-08-02 19:53:48 +0800

    Rename Machine_Learning_Pro/test1.py to ML_learn/test1.py

---
+++ ML_learn/test1.py
@@ -0,0 +1,42 @@
+ 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+ 
+path = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+ 
+headernames = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+
+ 
+dataset = pd.read_csv(path, names=headernames)
+dataset.head()
+
+ 
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+ 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+
+ 
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=50)
+classifier.fit(X_train, y_train)
+
+ 
+y_pred = classifier.predict(X_test)
+
+ 
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+result = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(result)
+result1 = classification_report(y_test, y_pred)
+print("Classification Report:",)
+print (result1)
+result2 = accuracy_score(y_test,y_pred)
+print("Accuracy:",result2)
+
