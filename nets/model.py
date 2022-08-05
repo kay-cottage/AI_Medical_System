ad3f9c376892053dd23f51863bd694fdb40cac4d
kay的小屋 <47267728+kay-cottage@users.noreply.github.com>
2022-08-05 16:29:59 +0800

    Add files via upload

--- nets/model.py
+++ nets/model.py
@@ -1,63 +1,181 @@
-# by kayak.gw
-from nets.transformer_text_encoder import text_encoder
-from nets.images_encoder import img_encoder
+from typing import List, Callable
+
 import torch
+from torch import Tensor
 import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.img_encoder=img_encoder()
-        self.text_encoder=text_encoder()
+def channel_shuffle(x: Tensor, groups: int) -> Tensor:
 
-        self.fc1 = nn.Linear(1024, 256)                                      # 设置第一个全连接层(输入层到隐藏层): 状态数个神经元到50个神经元
-        self.fc1.weight.data.normal_(0, 0.1)                                    # 权重初始化 (均值为0，方差为0.1的正态分布)
-        self.out = nn.Linear(256, 10)                                     # 设置第二个全连接层(隐藏层到输出层): 50个神经元到动作数个神经元
-        self.out.weight.data.normal_(0, 0.1)
-        
-    def forward(self,img,text):
-        img_feature=self.img_encoder(img)
-        text_feature=self.text_encoder(text)
+    batch_size, num_channels, height, width = x.size()
+    channels_per_group = num_channels // groups
 
-        x=torch.add(img_feature,text_feature)
-        x=F.relu(self.fc1(x))
-        x=self.out(x)
-        
+    # reshape
+    # [batch_size, num_channels, height, width] -> [batch_size, groups, channels_per_group, height, width]
+    x = x.view(batch_size, groups, channels_per_group, height, width)
+
+    x = torch.transpose(x, 1, 2).contiguous()
+
+    # flatten
+    x = x.view(batch_size, -1, height, width)
+
+    return x
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, input_c: int, output_c: int, stride: int):
+        super(InvertedResidual, self).__init__()
+
+        if stride not in [1, 2]:
+            raise ValueError("illegal stride value.")
+        self.stride = stride
+
+        assert output_c % 2 == 0
+        branch_features = output_c // 2
+        # 当stride为1时，input_channel应该是branch_features的两倍
+        # python中 '<<' 是位运算，可理解为计算×2的快速方法
+        assert (self.stride != 1) or (input_c == branch_features << 1)
+
+        if self.stride == 2:
+            self.branch1 = nn.Sequential(
+                self.depthwise_conv(input_c, input_c, kernel_s=3, stride=self.stride, padding=1),
+                nn.BatchNorm2d(input_c),
+                nn.Conv2d(input_c, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(branch_features),
+                nn.ReLU(inplace=True)
+            )
+        else:
+            self.branch1 = nn.Sequential()
+
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(input_c if self.stride > 1 else branch_features, branch_features, kernel_size=1,
+                      stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(branch_features),
+            nn.ReLU(inplace=True),
+            self.depthwise_conv(branch_features, branch_features, kernel_s=3, stride=self.stride, padding=1),
+            nn.BatchNorm2d(branch_features),
+            nn.Conv2d(branch_features, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(branch_features),
+            nn.ReLU(inplace=True)
+        )
+
+    @staticmethod
+    def depthwise_conv(input_c: int,
+                       output_c: int,
+                       kernel_s: int,
+                       stride: int = 1,
+                       padding: int = 0,
+                       bias: bool = False) -> nn.Conv2d:
+        return nn.Conv2d(in_channels=input_c, out_channels=output_c, kernel_size=kernel_s,
+                         stride=stride, padding=padding, bias=bias, groups=input_c)
+
+    def forward(self, x: Tensor) -> Tensor:
+        if self.stride == 1:
+            x1, x2 = x.chunk(2, dim=1)
+            out = torch.cat((x1, self.branch2(x2)), dim=1)
+        else:
+            out = torch.cat((self.branch1(x), self.branch2(x)), dim=1)
+
+        out = channel_shuffle(out, 2)
+
+        return out
+
+
+class ShuffleNetV2(nn.Module):
+    def __init__(self,
+                 stages_repeats: List[int],
+                 stages_out_channels: List[int],
+                 num_classes: int = 1000,
+                 inverted_residual: Callable[..., nn.Module] = InvertedResidual):
+        super(ShuffleNetV2, self).__init__()
+
+        if len(stages_repeats) != 3:
+            raise ValueError("expected stages_repeats as list of 3 positive ints")
+        if len(stages_out_channels) != 5:
+            raise ValueError("expected stages_out_channels as list of 5 positive ints")
+        self._stage_out_channels = stages_out_channels
+
+        # input RGB image
+        input_channels = 3
+        output_channels = self._stage_out_channels[0]
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(output_channels),
+            nn.ReLU(inplace=True)
+        )
+        input_channels = output_channels
+
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # Static annotations for mypy
+        self.stage2: nn.Sequential
+        self.stage3: nn.Sequential
+        self.stage4: nn.Sequential
+
+        stage_names = ["stage{}".format(i) for i in [2, 3, 4]]
+        for name, repeats, output_channels in zip(stage_names, stages_repeats,
+                                                  self._stage_out_channels[1:]):
+            seq = [inverted_residual(input_channels, output_channels, 2)]
+            for i in range(repeats - 1):
+                seq.append(inverted_residual(output_channels, output_channels, 1))
+            setattr(self, name, nn.Sequential(*seq))
+            input_channels = output_channels
+
+        output_channels = self._stage_out_channels[-1]
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(input_channels, output_channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(output_channels),
+            nn.ReLU(inplace=True)
+        )
+
+        self.fc = nn.Linear(output_channels, num_classes)
+
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+        x = self.conv5(x)
+        x = x.mean([2, 3])  # global pool
+        x = self.fc(x)
         return x
 
+    def forward(self, x: Tensor) -> Tensor:
+        return self._forward_impl(x)
 
-class net(nn.Module):
-    def __init__(self):
-        super(net, self).__init__()
-        self.img_encoder=img_encoder()
-        self.text_encoder=text_encoder()
 
-        self.fc1 = nn.Linear(1024, 256)                                      # 设置第一个全连接层(输入层到隐藏层): 状态数个神经元到50个神经元
-        self.fc1.weight.data.normal_(0, 0.1)                                    # 权重初始化 (均值为0，方差为0.1的正态分布)
-        self.out = nn.Linear(256, 10)                                     # 设置第二个全连接层(隐藏层到输出层): 50个神经元到动作数个神经元
-        self.out.weight.data.normal_(0, 0.1)
-        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
-        
-    def forward(self,img,text):
-        img_feature=self.img_encoder(img)
-        text_feature=self.text_encoder(text)
-        image_features = img_feature / img_feature.norm(dim=-1, keepdim=True)
-        text_features = text_feature / text_feature.norm(dim=-1, keepdim=True)
+def shufflenet_v2_x1_0(num_classes=1000):
+    """
+    Constructs a ShuffleNetV2 with 1.0x output channels, as described in
+    `"ShuffleNet V2: Practical Guidelines for Efficient CNN Architecture Design"
+    <https://arxiv.org/abs/1807.11164>`.
+    weight: https://download.pytorch.org/models/shufflenetv2_x1-5666bf0f80.pth
 
-        #logit_scale = self.logit_scale.exp()
-        #logits_per_image = logit_scale * image_features @ text_features.t()
-        #logits_per_text = logits_per_image.t()
-        x = (100.0 * image_features @ text_features.T).softmax(dim=-1)*self.logit_scale
-        return x
+    :param num_classes:
+    :return:
+    """
+    model = ShuffleNetV2(stages_repeats=[4, 8, 4],
+                         stages_out_channels=[24, 116, 232, 464, 1024],
+                         num_classes=num_classes)
 
-    
-'''
-img=torch.rand(1,3,224,224)
-text=torch.rand(1,77).type(torch.LongTensor)
-model=Net()
-a=model(img,text)
-print(a)
-'''
+    return model
+
+
+def shufflenet_v2_x0_5(num_classes=1000):
+    """
+    Constructs a ShuffleNetV2 with 0.5x output channels, as described in
+    `"ShuffleNet V2: Practical Guidelines for Efficient CNN Architecture Design"
+    <https://arxiv.org/abs/1807.11164>`.
+    weight: https://download.pytorch.org/models/shufflenetv2_x0.5-f707e7126e.pth
+
+    :param num_classes:
+    :return:
+    """
+    model = ShuffleNetV2(stages_repeats=[4, 8, 4],
+                         stages_out_channels=[24, 48, 96, 192, 1024],
+                         num_classes=num_classes)
+
+    return model
